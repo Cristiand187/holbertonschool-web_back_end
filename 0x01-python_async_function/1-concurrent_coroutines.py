6e5f7a1a2789d35be05c9ed25de28712ecdfdbb3
cristiand187 <cristiand187@hotmail.com>
2022-04-26 12:40:39 -0500

    Add Task 1

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -1,5 +1,10 @@
 #!/usr/bin/env python3
-"""  """
+""" 
+Import wait_random from the previous python file that you’ve 
+written and write an async routine called wait_n that takes 
+in 2 int arguments (in this order): n and max_delay. You will 
+spawn wait_random n times with the specified max_delay
+ """
 import asyncio
 from typing import List
 wait_random = __import__('0-basic_async_syntax').wait_random
